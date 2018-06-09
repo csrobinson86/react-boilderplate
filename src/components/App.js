dc5ada566f5d29283551c1e3724380b43d6dfbc0
Casey Robinson <casey.robinson@firespring.com>
2018-06-09 14:42:31 -0500

    webpack setup

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../styles/App.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>My React App!</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
